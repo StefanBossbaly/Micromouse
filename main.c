2f3d18585662cabeab8cd933c26a9ca76e57f291
Stefan Bossbaly <sbossb@gmail.com>
2013-03-31 15:47:44 -0400

    Forgot that we need to do things by reference in C
    I'm an idiot. We need the memory address of the struct to change
    its contents not copying it over to the stack.

--- main.c
+++ main.c
@@ -7,9 +7,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "wall.h"
 
 int main(int argc, char** argv) {
-
+    struct nav_cell cells[256];
+    struct nav_array nav;
+    nav.cells = cells;
+    nav.length = 16;
+    nav.width = 16;
+    
+    nav.cells[0].flood_num = -1;
+    
+    nav_init(&nav);
+    
+    
+    int i;
+    
+    for (i = 0; i < 256; i++)
+    {
+        struct nav_cell cell = nav.cells[i];
+        printf("%i\n", cell.flood_num);
+    }
+    
     return (EXIT_SUCCESS);
 }
 
