46062aeaf6ecb5cda268a7adaa4f4a4b2c5ec7b0
Stefan Bossbaly <sbossb@gmail.com>
2013-04-01 14:03:54 -0400

    Added new subroutines to print out walls
    This will help with debugging the wall aspect of the nav array.
    Anyway I hope to make this better in the future.

--- main.c
+++ main.c
@@ -23,6 +23,23 @@ void print_nav(struct nav_array *nav)
     }
 }
 
+void print_wall(struct nav_array *nav)
+{
+    int i,j;
+    for (i = 0; i < nav->width; i++)
+    {
+        for (j = 0; j < nav->length; j++)
+        {
+            struct nav_cell *cell = nav_get_cell(nav, i, j);
+            if (cell->north || cell->east || cell->south || cell->west)
+            {
+                printf("Index: (%i, %i) (%i, %i, %i, %i)", i, j, cell->north, cell->east, cell->south, cell->west);
+                printf("\n");
+            }
+        };
+    }
+}
+
 int main(int argc, char** argv) {
     struct nav_cell cells[256];
     struct nav_array nav;
@@ -30,14 +47,22 @@ int main(int argc, char** argv) {
     nav.length = 16;
     nav.width = 16;
     
+    pos_t pos;
+    pos.column = 0;
+    pos.row = 0;
+    pos.direction = north;
+    
     nav_init(&nav);
     print_nav(&nav);
     
-    nav_flood(&nav);
+    /*nav_flood(&nav);
     print_nav(&nav);
     
     nav_reset_visited(&nav);
-    print_nav(&nav);
+    print_nav(&nav);*/
+    
+    nav_update_wall(&nav, &pos, right);
+    print_wall(&nav);
     
     return (EXIT_SUCCESS);
 }
