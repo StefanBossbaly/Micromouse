0542f648863e141ee8b8e72fd9124157fcafd702
Stefan Bossbaly <Sbossb@gmail.com>
2013-04-05 12:07:56 -0400

    Added subroutine to check if nav cell is in queue
    This will solve the stupid overflow error :)

--- queue.h
+++ queue.h
@@ -20,6 +20,7 @@ void nav_queue_init(nav_queue *queue, nav_queue_cell *cells, int capicity);
 void nav_queue_enqueue(nav_queue *queue, struct nav_cell *cell, int row, int column, int n);
 void nav_queue_dequeue(nav_queue *queue, nav_queue_cell *buffer);
 int nav_queue_empty(nav_queue *queue);
+int nav_queue_is_queued(nav_queue *queue, struct nav_cell *cell);
 
 #endif	/* QUEUE_H */
 
