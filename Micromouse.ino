76279facea25aa5ccf1cec29bb806f60955c7c9b
Stefan Bossbaly <sbossb@gmail.com>
2014-02-23 14:59:11 -0500

    New motors are working :)

--- Micromouse.ino
+++ Micromouse.ino
@@ -2,84 +2,11 @@
 #include "queue.h"
 #include "position.h"
 #include "timer.h"
-#include "Adafruit_MotorShield.h"
+#include "stepper.h"
 #include "detection.h"
-
-/*void print_nav(struct nav_array *nav)
-{
-	char temp[20];
-
-    int i,j;
-    for (i = 0; i < nav->rows; i++)
-    {
-        for (j = 0; j < nav->columns; j++)
-        {
-            struct nav_cell *cell = nav_get_cell(nav, i, j);
-
-            if (cell->flood_num < 10)
-            {
-            	Serial.print('0');
-            	Serial.print(cell->flood_num);
-            }
-            else
-            {
-            	Serial.print(cell->flood_num);
-            }
-        }
-        Serial.print("\n");
-    }
-}
-
-void load_test_maze_walls(struct nav_array *nav, pos_t *pos)
-{
-    pos->row = 5;
-    pos->column = 2;
-    pos->direction = north;
-    
-    nav_update_wall(nav, pos, front);
-    
-    pos->column = 1;
-    pos->row = 4;
-    
-    nav_update_wall(nav, pos, left);
-    nav_update_wall(nav, pos, front);
-    
-    pos->column = 2;
-    nav_update_wall(nav, pos, front);
-    
-    pos->row = 3;
-    pos->column = 1;
-    nav_update_wall(nav, pos, left);
-    
-    pos->row = 2;
-    nav_update_wall(nav, pos, left);
-    nav_update_wall(nav, pos, front);
-    
-    pos->row = 1;
-    pos->column = 0;
-    nav_update_wall(nav, pos, front);
-    
-    pos->column = 1;
-    nav_update_wall(nav, pos, right);
-    
-    
-    pos->row = 5;
-    pos->column = 2;
-    pos->direction = north;
-}*/
 
 volatile unsigned long start_time = 0;
 volatile uint8_t stop = 0; 
-
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x60);
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
-
-// Connect a stepper motor with 200 steps per revolution (1.8 degree)
-// to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
-Adafruit_StepperMotor *myMotor2 = AFMS.getStepper(200, 1);
-
 
 volatile int s0 = -1, s1 = -1, s2 = -1, s3 = -1, s4 = -1;
 
@@ -186,6 +113,9 @@ void handle_sensors (void)
 
 int on_off = 0;
 
+shield_t shield;
+stepper_t motor0, motor1;
+
 // Micromouse.ino
 void setup() 
 {
@@ -196,31 +126,27 @@ void setup()
     // Start a serial with 115200 baud rate
     Serial.begin(115200);
 
-    // Start the pwm
-    //AFMS.begin(1000);
-
     //handle_sensors();
 
     // Start a callback to the sensors
     //timer2_init_ms(500, handle_sensors);
     //start_time = millis();
+    
+    shield_init(&shield, 0x60);
+    
+    shield_begin(&shield, 1600);
+    
+    stepper_init(&motor0, &shield, 0);
+    stepper_init(&motor1, &shield, 1);
 }
 
 void loop() 
 {
-	Serial.println(dectection_relay_position());
-	on_off = (on_off + 1) % 2;
-	
-	if (on_off == 0)
+	for (int i = 0; i < 85; i++)
 	{
-		dectection_switch_relay(3);
-	}
-	else
-	{
-		dectection_switch_relay(4);
+		stepper_step(&motor0, FORWARD);
+		stepper_step(&motor1, BACKWARD);
 	}
 	
-	Serial.println(dectection_relay_position());
-	
-    delay(2000);
+	delay(3000);
 }
