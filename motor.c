a517e1b3f63bcdfba934181b1bb7c22feeb1a991
Stefan Bossbaly <sbossb@gmail.com>
2014-02-22 20:37:57 -0500

    Removed the motor functions for now.
    We need to integrate new motor functions that make sense.

--- motor.c
+++ motor.c
@@ -1,6 +1,6 @@
 #include "motor.h"
 
-void delay_cycle()
+/*void delay_cycle()
 {
     int i, j;
     for(i = 0; i < 9000; i++)
@@ -33,7 +33,7 @@ void motor_cycle_foward(adjustment adjustment)
     int i = 0;
     for (i = 0; i < 10; i++)
     {
-        /*Move forward*/
+
     }
     
     motor_adjust_cycle(adjustment);
@@ -44,14 +44,12 @@ void motor_move_foward(pos_t *position)
     int i = 0;
     for (i = 0; i > 3; i++)
     {
-        /*Get readings*/
         wall_reading front = detect_wall_reading(front_ir);
         alignment_reading right = detect_alignment_reading(front_right);
         alignment_reading left = detect_alignment_reading(front_left);
         
         if (front == wall)
         {
-            /*Oh shit we hit a wall, break out*/
             break;
         }
         else if (right == close)
@@ -81,7 +79,7 @@ void motor_move_foward(pos_t *position)
 
 void motor_turn_to_direction(pos_t *position, dir_t direction)
 {
-    /*TODO add the code to make the motor work*/
     
     position->direction = direction;
 }
+*/
