aa715c1121d8b72097d40f129d319c1f8a6f5919
Stefan Bossbaly <sbossb@gmail.com>
2014-02-24 17:59:15 -0500

    Added shared values
    We need to share values between the detection functions and the
    motor functions. Because of this we need to setup shared values.
    They will be kept in the motor.h header file. The var motor_status
    can only be changed by the motor functions and motor_adjustment
    can only be changed by the dectection functions. This why we will
    not run into timing issues.

--- motor.h
+++ motor.h
@@ -7,15 +7,34 @@
 
 #define TURN_ADJ 82
 #define TURN_180 164
+#define BLOCK 233
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
+// Status of the motor
+#define MOTOR_STANDBY 0
+#define MOTOR_MOVING 1
+#define MOTOR_TURNING 2
+#define MOTOR_MOVING 3
+
+//Slight right adjustment
+#define MOTOR_NO_ADJ 0
+#define MOTOR_S_R_ADJ 1
+#define MOTOR_S_L_ADJ 2
+#define MOTOR_H_R_ADJ 3
+#define MOTOR_H_L_ADJ 4
+
+// Shared values
+extern volatile int motor_status = -1;
+extern volatile int motor_adjustment = -1;
+
 void motor_turn_left(stepper_t *stepper0, stepper_t *stepper1);
 void motor_turn_right(stepper_t *stepper0, stepper_t *stepper1);
 void motor_turn_180(stepper_t *stepper0, stepper_t *stepper1);
+void motor_move_forward(stepper_t *stepper0, stepper_t *stepper1);
 
 #ifdef __cplusplus
 }
