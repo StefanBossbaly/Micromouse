6bb620c9ff7ba30c58d71d4b5c5012ebe8a535d5
Stefan Bossbaly <sbossb@gmail.com>
2014-02-25 14:40:22 -0500

    Fixed turning
    Opps turning left turned right and turning right turned left.

--- motor.c
+++ motor.c
@@ -11,8 +11,8 @@ void motor_turn_left(stepper_t *stepper0, stepper_t *stepper1)
 
 	for (i = 0; i < TURN_ADJ; i++)
 	{
-		stepper_step(stepper0, FORWARD);
-		stepper_step(stepper1, BACKWARD);
+		stepper_step(stepper0, BACKWARD);
+		stepper_step(stepper1, FORWARD);
 	}
 
 	motor_status = MOTOR_STANDBY;
@@ -26,8 +26,8 @@ void motor_turn_right(stepper_t *stepper0, stepper_t *stepper1)
 
 	for (i = 0; i < TURN_ADJ; i++)
 	{
-		stepper_step(stepper0, BACKWARD);
-		stepper_step(stepper1, FORWARD);
+		stepper_step(stepper0, FORWARD);
+		stepper_step(stepper1, BACKWARD);
 	}
 
 	motor_status = MOTOR_STANDBY;
@@ -55,6 +55,11 @@ void motor_move_forward(stepper_t *stepper0, stepper_t *stepper1)
 	int i;
 	for (i = 1; i < BLOCK; i++)
 	{
+		if (motor_adjustment == MOTOR_WALL)
+		{
+			break;
+		}
+
 		stepper_step(stepper0, FORWARD);
 
 		if (motor_adjustment == MOTOR_S_R_ADJ && (i % 8) == 0)
